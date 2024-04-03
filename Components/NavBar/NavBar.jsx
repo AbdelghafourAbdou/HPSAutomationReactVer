8aec5d88f668d74422f113984044bfcd174e6097
Abdelghafour Abdou <abdelghafourabdou@gmail.com>
2024-04-03 23:44:21 +0000

    updated buttons and dark mode April 3rd

--- Components/NavBar/NavBar.jsx
+++ Components/NavBar/NavBar.jsx
@@ -10,6 +10,11 @@ export default function NavBar() {
     function handleClick(e) {
         const id = e.target.id;
         let newArray = Array(6).fill(0);
+
+        if (openTab[id]) {
+            return setOpenTab(newArray);
+        }
+
         newArray[id] = 1;
         setOpenTab(newArray);
     }
@@ -24,40 +29,34 @@ export default function NavBar() {
             </h5>
             <nav className="navbar">
                 <button className='buttonContainer' id='0' onClick={handleClick}>
-                    <div className='navButton'>
-                        <MdDashboard />Dashboard
-                    </div>
+                    <i className='buttonIcon'><MdDashboard /></i>
+                    Dashboard
                     <Link open={openTab}>{[0, 'https://www.google.com/', 'Global Statistics']}</Link>
                 </button>
                 <button className='buttonContainer' id='1' onClick={handleClick}>
-                    <article className='navButton'>
-                        <FaStream />API Stream
-                    </article>
-                    <Link open={openTab}>{[1, ['stat', 'proj', 'ws', 'tc', 'ts'], ['Statistics', 
-                            'Projects', 'Web Services', 'Test Cases', 'Test Suits']]}</Link>
+                    <i className='buttonIcon'><FaStream /></i>
+                    API Stream
+                    <Link open={openTab}>{[1, ['stat', 'proj', 'ws', 'tc', 'ts'], ['Statistics',
+                        'Projects', 'Web Services', 'Test Cases', 'Test Suits']]}</Link>
                 </button>
                 <button className='buttonContainer' id='2' onClick={handleClick}>
-                    <article className='navButton'>
-                        <FaStream />GUI Stream
-                    </article>
+                    <i className='buttonIcon'><FaStream /></i>
+                    GUI Stream
                     <Link open={openTab}>{[2, '/', 'Lorem Ipsum']}</Link>
                 </button>
                 <button className='buttonContainer' id='3' onClick={handleClick}>
-                    <article className='navButton'>
-                        <FaStream />Online Stream
-                    </article>
+                    <i className='buttonIcon'><FaStream /></i>
+                    Online Stream
                     <Link open={openTab}>{[3, '/', 'Lorem Ipsum']}</Link>
                 </button>
                 <button className='buttonContainer' id='4' onClick={handleClick}>
-                    <article className='navButton'>
-                        <FaStream />Batch Stream
-                    </article>
+                    <i className='buttonIcon'><FaStream /></i>
+                    Batch Stream
                     <Link open={openTab}>{[4, '/', 'Lorem Ipsum']}</Link>
                 </button>
                 <button className='buttonContainer' id='5' onClick={handleClick}>
-                    <article className='navButton'>
-                        <MdAdminPanelSettings />Administration
-                    </article>
+                    <i className='buttonIcon'><MdAdminPanelSettings /></i>
+                    Administration
                     <Link open={openTab}>{[5, '/', 'Gonfigurations']}</Link>
                 </button>
             </nav>
