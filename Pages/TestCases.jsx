ad9eced70fe6dbbb15ca9aec81650a24224c5c7b
Abdelghafour Abdou <abdelghafourabdou@gmail.com>
2024-04-04 22:37:55 +0000

    updated testCases page April 4th

--- Pages/TestCases.jsx
+++ Pages/TestCases.jsx
@@ -1,10 +1,82 @@
-import { } from 'react';
+import { Form, useNavigation } from 'react-router-dom';
 import './TestCases.css'
 
+// eslint-disable-next-line react-refresh/only-export-components
+export async function action({ request }) {
+    const formData = await request.formData();
+    const project = formData.get('project');
+    const status = formData.get('status');
+    const testCase = formData.get('testCase');
+    const webService = formData.get('webService');
+    const wsVersion = formData.get('wsVersion');
+
+    const req = { project, status, testCase, webService, wsVersion };
+    try {
+        const res = await fetch('http://localhost:8088/pwcAutomationTest/searchTestCases?page=0&size=6',
+            { method: 'POST', body: JSON.stringify(req) });
+
+        if (!res.ok) {
+            throw new Error;
+        }
+
+        const data = await res.json();
+        return data;
+    } catch (error) {
+        return error;
+    }
+}
+
 export default function TestCases() {
+    const navigate = useNavigation();
+    const status = navigate.state;
+
     return (
-        <div>
-            TestCases
+        <div className='testCasesContainer'>
+            <div className='titleContainer'>
+                <h1>Test Cases</h1>
+            </div>
+            <Form method='post' className='testCaseFormContainer' replace>
+                <div className='fieldContainer'>
+                    <label htmlFor='projectSelection'>
+                        Project:
+                    </label>
+                    <select id='projectSelection' name='project'>
+                        <option value="" key="0">All</option>
+                        <option value="COSMOTE" key="1">COSMOTE</option>
+                        <option value="GOHENRY" key="2">GOHENRY</option>
+                    </select>
+                </div>
+                <div className='fieldContainer'>
+                    <label>
+                        Web Service:
+                    </label>
+                    <input type="text" name='webService' placeholder='Enter Web Service' />
+                </div>
+                <div className='fieldContainer'>
+                    <label>
+                        Ws Version:
+                    </label>
+                    <input type="text" name='wsVersion' placeholder='Enter Version' />
+                </div>
+                <div className='fieldContainer'>
+                    <label>
+                        Test Case:
+                    </label>
+                    <input type="text" name='testCase' placeholder='Enter Test Case' />
+                </div>
+                <div className='fieldContainer'>
+                    <label htmlFor='statusSelection'>
+                        Status:
+                    </label>
+                    <select id='statusSelection' name='status'>
+                        <option value="" key="0">All</option>
+                        <option value="PASSED" key="1">PASSED</option>
+                        <option value="FAILED" key="2">FAILED</option>
+                        <option value="READY" key="3">READY</option>
+                    </select>
+                </div>
+                <button disabled={status === 'submitting'}>{status === 'submitting' ? 'Searching...' : 'Search'}</button>
+            </Form>
         </div>
     )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { } from 'react';
 import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
-import TestCases from '../Pages/TestCases';
+import TestCases, { action as testCasesAction } from '../Pages/TestCases';
 import Administration from '../Pages/Administration';
 import { action as emailAction } from '../Pages/Configs/EmailConfig';
 import Layout from '../Layouts/Layout';
@@ -12,7 +12,7 @@ function App() {
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
         <Route path='administration' element={<Administration />} action={emailAction} />
-        <Route path='testCases'element={<TestCases />} />
+        <Route path='testCases' element={<TestCases />} action={testCasesAction} />
       </Route>
     )
   );
