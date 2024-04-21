e2395aa5e49a8a28076e60f448713bf380a18678
Abdelghafour Abdou <abdelghafourabdou@gmail.com>
2024-04-21 22:44:24 +0100

    updated CardMGT 21th

--- Pages/CardMGT/CardMGT.jsx
+++ Pages/CardMGT/CardMGT.jsx
@@ -1,4 +1,9 @@
 import { useState, useEffect } from 'react';
+import {
+    getFormattedCurrentDateTime, generateSixRandomNumbers,
+    getFormattedCurrentDate, getFormattedCurrentTime,
+    generateElevenNumbers, getJulianFormattedDate
+} from '../../Utils/Utils';
 import './CardMGT.css';
 
 const BASEPATH = 'http://localhost:8088/pwcAutomationTest/DataBase';
@@ -6,6 +11,10 @@ const BASEPATH = 'http://localhost:8088/pwcAutomationTest/DataBase';
 export default function CardMGT() {
     const [latestCard,] = useState(localStorage.getItem('latestCard'));
     const [results, setResults] = useState(Array(4).fill(null));
+    const [baseTestDetails, setBaseTestDetails] = useState({
+        baseMessageStringRef: '',
+        msgTypeStringRef: '',
+    });
 
     // activate the card
     async function handleActivateCard() {
@@ -25,6 +34,54 @@ export default function CardMGT() {
         setResults(prev => [prev[0], data, ...(prev.slice(2))]);
     }
 
+    // create a base test file for testing
+    async function handleBaseTest() {
+        const field11 = generateSixRandomNumbers();
+        const field32 = generateElevenNumbers();
+        const message = {
+            '002': latestCard,
+            '003': 200000,
+            '004': String(40.00 * 100).padStart(12, '0'),
+            '006': String(40.00 * 100).padStart(12, '0'),
+            '007': getFormattedCurrentDateTime(),
+            '011': field11,
+            '012': getFormattedCurrentTime(),
+            '013': getFormattedCurrentDate(),
+            '014': 2801,
+            '018': 5999,
+            '019': 380,
+            '022': '0210',
+            '023': '001',
+            '025': '00',
+            '032': field32,
+            '035': '',  // to be filled with a query from db
+            '037': `${getJulianFormattedDate()}${field11}`,
+            '041': 'TERMID01',
+            '042': 'CARD ACCEPTOR  ',
+            '043': 'CARD ACCEPTOR TESTDATA   CASABLANCA   MA',
+            '049': 978,
+            '051': 978,
+            '052': '058CEFE99578CEDD',
+            '053': '2001010100000000',
+            '055': '0100669F3303204000950580000100009F37049BADBCAB9F1E08F9F9F9F9F9F9F9F99F100C0B010A03A0B00000000000009F26080123456789ABCDEF9F36020104820200009C01009F1A0208409A030101019F02060000000123005F2A0208409F0306000000000000',
+            '060': 45,
+            '062': '0000000000000000',
+            '063': '8000000002',
+            '090': `01000000000000000000${field32}00000000000`,
+            '123': ' 10000127,Street Av'
+        }
+        const headers = new Headers({ 'Content-Type': 'application/json' });
+        const res = await fetch(`${BASEPATH}/addBaseTest?baseMessageString=${baseTestDetails.baseMessageStringRef}&msgTypeString=${baseTestDetails.msgTypeStringRef}&msgHeaderString=16010200FE0000000000000000000000000000000000`,
+            { method: 'POST', headers, body: JSON.stringify(message) });
+        const data = await res.json();
+        setResults(prev => [prev[0], prev[1], data, prev[3]]);
+    }
+
+    function handleFormDataChange(e) {
+        setBaseTestDetails(prev => ({
+            ...prev, [e.target.name]: e.target.value
+        }));
+    }
     useEffect(() => {
         console.log(results);
     }, [results]);
@@ -47,8 +104,18 @@ export default function CardMGT() {
                         <p>{results[1] === null ? 'No Updates' : results[1].indexOf('<CardProfile') !== -1 ? 'Card Profile Added Successfuly' : results[1]}</p>
                     </div>
                     <div className='controlPanelButton'>
-                        <button>Add Base Test</button>
-                        <p>No Updates</p>
+                        <input type="text" name='baseMessageStringRef' value={baseTestDetails.baseMessageStringRef}
+                            onChange={handleFormDataChange} placeholder='Enter Base Test Name'/>
+                        <select name='msgTypeStringRef' value={baseTestDetails.msgTypeStringRef}
+                            onChange={handleFormDataChange}>
+                            <option value="100" key="0">100</option>
+                            <option value="120" key="1">120</option>
+                            <option value="121" key="2">121</option>
+                            <option value="400" key="3">400</option>
+                            <option value="420" key="4">420</option>
+                        </select>
+                        <button onClick={handleBaseTest}>Add Base Test</button>
+                        <p>{results[2] === null ? 'No Updates' : results[2] ? 'Base Test Added Successfully' : 'Base Test Addition Failed'}</p>
                     </div>
                     <div className='controlPanelButton'>
                         <button>Create MSG File</button>
--- Utils/Utils.jsx
+++ Utils/Utils.jsx
@@ -4,3 +4,61 @@ export const toProper = (title) => {
     let newTitle = tempArr.toSpliced(0, 1, capitalFirstLetter).join('');
     return newTitle;
 }
+
+export const getFormattedCurrentDateTime = () => {
+    const now = new Date();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${month}${day}${hours}${minutes}${seconds}`;
+}
+
+export const getFormattedCurrentDate = () => {
+    const now = new Date();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Month is zero-based
+    const day = String(now.getDate()).padStart(2, '0');
+
+    return `${month}${day}`;
+}
+
+export const getFormattedCurrentTime = () => {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+
+    return `${hours}${minutes}${seconds}`;
+}
+
+export const generateSixRandomNumbers = () => {
+    let concatenatedNumbers = '';
+    for (let i = 0; i < 6; i++) {
+        const randomNum = Math.floor(Math.random() * 10); // Generates random number between 0 and 9
+        concatenatedNumbers += randomNum;
+    }
+    return concatenatedNumbers;
+}
+
+export const generateElevenNumbers = () => {
+    let concatenatedNumbers = '';
+    for (let i = 0; i < 11; i++) {
+        const randomNum = Math.floor(Math.random() * 10); // Generates random number between 0 and 9
+        concatenatedNumbers += randomNum;
+    }
+    return concatenatedNumbers;
+}
+
+export const getJulianFormattedDate = () => {
+    const now = new Date();
+    const years = String(now.getFullYear());
+    const startOfYear = new Date(now.getFullYear(), 0, 0);
+    const diff = now - startOfYear;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const lastYearChar = years[years.length - 1];
+    const daysSinceStartOfYear = String(Math.floor(diff / oneDay)).padStart(3, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    return `${lastYearChar}${daysSinceStartOfYear}${hours}`;
+}
