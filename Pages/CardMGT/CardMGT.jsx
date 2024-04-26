a72eb12e37afc5cc81b6f3fbdc3a407b02539328
Abdelghafour Abdou <abdelghafourabdou@gmail.com>
2024-04-26 01:19:53 +0100

    changed design to focus on MSG, April 26th

--- Pages/CardMGT/CardMGT.jsx
+++ Pages/CardMGT/CardMGT.jsx
@@ -1,26 +1,36 @@
 import { useState, useEffect } from 'react';
-import {
-    getFormattedCurrentDateTime, generateSixRandomNumbers,
-    getFormattedCurrentDate, getFormattedCurrentTime,
-    generateElevenNumbers, getJulianFormattedDate,
-    toProperMultipleWords, generateConsequentNumber
-} from '../../Utils/Utils';
+import { useLoaderData } from 'react-router-dom';
+// import {
+//     getFormattedCurrentDateTime, generateSixRandomNumbers,
+//     getFormattedCurrentDate, getFormattedCurrentTime,
+//     generateElevenNumbers, getJulianFormattedDate,
+//     toProperMultipleWords, generateConsequentNumber
+// } from '../../Utils/Utils';
 import './CardMGT.css';
 
 const BASEPATH = 'http://localhost:8088/pwcAutomationTest/DataBase';
 
+// eslint-disable-next-line react-refresh/only-export-components
+export async function loader() {
+    const res = await fetch(`${BASEPATH}/baseMessages`);
+    const data = await res.text();
+    return data;
+}
+
 export default function CardMGT() {
+    const laoderData = useLoaderData();
     const [latestCard,] = useState(localStorage.getItem('latestCard'));
     const [results, setResults] = useState(Array(5).fill(null));
-    const [cardMGTDetails, setCardMGTDetails] = useState({
-        amount: 0,
-        baseMessageStringRef: '',
-        msgTypeStringRef: '',
-        MSGHeadline: '',
-        testId: null,
-    });
+    // const [cardMGTDetails, setCardMGTDetails] = useState({
+    //     amount: 0,
+    //     baseMessageStringRef: '',
+    //     msgTypeStringRef: '',
+    //     MSGHeadline: '',
+    //     testId: null,
+    // });
 
     // activate the card
+
     async function handleActivateCard() {
         setResults(prev => [null, ...(prev.slice(1))]);
         const headers = new Headers({ 'Content-Type': 'application/json' });
@@ -41,103 +51,103 @@ export default function CardMGT() {
         setResults(prev => [prev[0], data, ...(prev.slice(2))]);
     }
 
-    // create a base test file for testing
-    async function handleBaseTest() {
-        setResults(prev => [prev[0], prev[1], null, prev[3], prev[4]]);
-        const field11 = generateSixRandomNumbers();
-        const field32 = generateElevenNumbers();
-        const message = {
-            '002': latestCard,
-            '003': 200000,
-            '004': String(cardMGTDetails.amount).padStart(12, '0'),
-            '006': String(cardMGTDetails.amount).padStart(12, '0'),
-            '007': getFormattedCurrentDateTime(),
-            '011': field11,
-            '012': getFormattedCurrentTime(),
-            '013': getFormattedCurrentDate(),
-            '014': 2801,
-            '018': 5999,
-            '019': 380,
-            '022': '0210',
-            '023': '001',
-            '025': '00',
-            '032': field32,
-            '035': '',  // to be filled with a query from db
-            '037': `${getJulianFormattedDate()}${field11}`,
-            '041': 'TERMID01',
-            '042': 'CARD ACCEPTOR  ',
-            '043': 'CARD ACCEPTOR TESTDATA   CASABLANCA   MA',
-            '049': 978,
-            '051': 978,
-            '052': '058CEFE99578CEDD',
-            '053': '2001010100000000',
-            '055': '0100669F3303204000950580000100009F37049BADBCAB9F1E08F9F9F9F9F9F9F9F99F100C0B010A03A0B00000000000009F26080123456789ABCDEF9F36020104820200009C01009F1A0208409A030101019F02060000000123005F2A0208409F0306000000000000',
-            '060': 45,
-            '062': '0000000000000000',
-            '063': '8000000002',
-            '090': `01000000000000000000${field32}00000000000`,
-            '123': ' 10000127,Street Av'
-        }
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        const res = await fetch(`${BASEPATH}/addBaseTest?baseMessageString=${toProperMultipleWords(cardMGTDetails.baseMessageStringRef)}&msgTypeString=${String(cardMGTDetails.msgTypeStringRef).padStart(4, '0')}&msgHeaderString=16010200FE0000000000000000000000000000000000`,
-            { method: 'POST', headers, body: JSON.stringify(message, Object.keys(message).sort()) });
-        const data = await res.text();
-        const regex = new RegExp('\\d{3}_\\w+', 'g');
-        regex.test(data) ? localStorage.setItem('baseTest', data) : null;
-        setResults(prev => [prev[0], prev[1], data, prev[3], prev[4]]);
-    }
+    // // create a base test file for testing
+    // async function handleBaseTest() {
+    //     setResults(prev => [prev[0], prev[1], null, prev[3], prev[4]]);
+    //     const field11 = generateSixRandomNumbers();
+    //     const field32 = generateElevenNumbers();
+    //     const message = {
+    //         '002': latestCard,
+    //         '003': 200000,
+    //         '004': String(cardMGTDetails.amount).padStart(12, '0'),
+    //         '006': String(cardMGTDetails.amount).padStart(12, '0'),
+    //         '007': getFormattedCurrentDateTime(),
+    //         '011': field11,
+    //         '012': getFormattedCurrentTime(),
+    //         '013': getFormattedCurrentDate(),
+    //         '014': 2801,
+    //         '018': 5999,
+    //         '019': 380,
+    //         '022': '0210',
+    //         '023': '001',
+    //         '025': '00',
+    //         '032': field32,
+    //         '035': '',  // to be filled with a query from db
+    //         '037': `${getJulianFormattedDate()}${field11}`,
+    //         '041': 'TERMID01',
+    //         '042': 'CARD ACCEPTOR  ',
+    //         '043': 'CARD ACCEPTOR TESTDATA   CASABLANCA   MA',
+    //         '049': 978,
+    //         '051': 978,
+    //         '052': '058CEFE99578CEDD',
+    //         '053': '2001010100000000',
+    //         '055': '0100669F3303204000950580000100009F37049BADBCAB9F1E08F9F9F9F9F9F9F9F99F100C0B010A03A0B00000000000009F26080123456789ABCDEF9F36020104820200009C01009F1A0208409A030101019F02060000000123005F2A0208409F0306000000000000',
+    //         '060': 45,
+    //         '062': '0000000000000000',
+    //         '063': '8000000002',
+    //         '090': `01000000000000000000${field32}00000000000`,
+    //         '123': ' 10000127,Street Av'
+    //     }
+    //     const headers = new Headers({ 'Content-Type': 'application/json' });
+    //     const res = await fetch(`${BASEPATH}/addBaseTest?baseMessageString=${toProperMultipleWords(cardMGTDetails.baseMessageStringRef)}&msgTypeString=${String(cardMGTDetails.msgTypeStringRef).padStart(4, '0')}&msgHeaderString=16010200FE0000000000000000000000000000000000`,
+    //         { method: 'POST', headers, body: JSON.stringify(message, Object.keys(message).sort()) });
+    //     const data = await res.text();
+    //     const regex = new RegExp('\\d{3}_\\w+', 'g');
+    //     regex.test(data) ? localStorage.setItem('baseTest', data) : null;
+    //     setResults(prev => [prev[0], prev[1], data, prev[3], prev[4]]);
+    // }
 
-    // handle the input fields change
-    function handleFormDataChange(e) {
-        let { name, value } = e.target;
-        if (name === 'amount') {
-            value = Number(value);
-            if (!Number.isInteger(value)) {
-                value = Math.round(value * 100) / 100
-            }
-            value *= 100;
-        }
-        setCardMGTDetails(prev => ({
-            ...prev, [name]: value
-        }));
-    }
+    // // handle the input fields change
+    // function handleFormDataChange(e) {
+    //     let { name, value } = e.target;
+    //     if (name === 'amount') {
+    //         value = Number(value);
+    //         if (!Number.isInteger(value)) {
+    //             value = Math.round(value * 100) / 100
+    //         }
+    //         value *= 100;
+    //     }
+    //     setCardMGTDetails(prev => ({
+    //         ...prev, [name]: value
+    //     }));
+    // }
 
-    // handle the creation of MSG File using the newly created Card Profile and Base Test
-    async function handleMSGFile() {
-        setResults(prev => [prev[0], prev[1], prev[2], null, prev[4]]);
-        let cardProfile = localStorage.getItem('cardProfile');
-        let baseTest = localStorage.getItem('baseTest');
-        let consequentNumber = generateConsequentNumber();
-        let regex = new RegExp('(?<=_)([a-zA-Z]+)(?=_)');
-        const message = {
-            name: `MSG_${consequentNumber}_${baseTest}`,
-            baseTestName: baseTest,
-            caseId: consequentNumber,
-            sequence: Number(consequentNumber.slice(-3)),
-            cardProfile: cardProfile.slice(19, 39),
-            terminalProfile: 'MER_SHOP_ONUS_B1',
-            isPlayable: 1,
-            description: regex.exec(baseTest)[0],
-            headLine: cardMGTDetails.MSGHeadline,
-            rootCaseSpecValues: '',
-        }
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        const res = await fetch(`${BASEPATH}/addMSG`,
-            { method: 'POST', headers, body: JSON.stringify(message) });
-        const data = await res.text();
-        setResults(prev => [prev[0], prev[1], prev[2], data, prev[4]]);
-    }
+    // // handle the creation of MSG File using the newly created Card Profile and Base Test
+    // async function handleMSGFile() {
+    //     setResults(prev => [prev[0], prev[1], prev[2], null, prev[4]]);
+    //     let cardProfile = localStorage.getItem('cardProfile');
+    //     let baseTest = localStorage.getItem('baseTest');
+    //     let consequentNumber = generateConsequentNumber();
+    //     let regex = new RegExp('(?<=_)([a-zA-Z]+)(?=_)');
+    //     const message = {
+    //         name: `MSG_${consequentNumber}_${baseTest}`,
+    //         baseTestName: baseTest,
+    //         caseId: consequentNumber,
+    //         sequence: Number(consequentNumber.slice(-3)),
+    //         cardProfile: cardProfile.slice(19, 39),
+    //         terminalProfile: 'MER_SHOP_ONUS_B1',
+    //         isPlayable: 1,
+    //         description: regex.exec(baseTest)[0],
+    //         headLine: cardMGTDetails.MSGHeadline,
+    //         rootCaseSpecValues: '',
+    //     }
+    //     const headers = new Headers({ 'Content-Type': 'application/json' });
+    //     const res = await fetch(`${BASEPATH}/addMSG`,
+    //         { method: 'POST', headers, body: JSON.stringify(message) });
+    //     const data = await res.text();
+    //     setResults(prev => [prev[0], prev[1], prev[2], data, prev[4]]);
+    // }
 
-    async function handleRunSim() {
-        setResults(prev => [...(prev.slice(0, 4)), null]);
-        const message = {
-            'choice' : cardMGTDetails.testId,
-        }
-        const headers = new Headers({ 'Content-Type': 'application/json' });
-        await fetch(`${BASEPATH}/runSim`,
-            { method: 'POST', headers, body: JSON.stringify(message) });
-        setResults(prev => [...(prev.slice(0, 4)), "Simulation Done"]);
-    }
+    // async function handleRunSim() {
+    //     setResults(prev => [...(prev.slice(0, 4)), null]);
+    //     const message = {
+    //         'choice' : cardMGTDetails.testId,
+    //     }
+    //     const headers = new Headers({ 'Content-Type': 'application/json' });
+    //     await fetch(`${BASEPATH}/runSim`,
+    //         { method: 'POST', headers, body: JSON.stringify(message) });
+    //     setResults(prev => [...(prev.slice(0, 4)), "Simulation Done"]);
+    // }
 
     useEffect(() => {
         console.log(results);
@@ -162,7 +172,7 @@ export default function CardMGT() {
                             <p>{results[1] === null ? 'No Updates' : results[1].indexOf('<CardProfile') !== -1 ? 'Card Profile Added Successfully' : results[1]}</p>
                         </div>
                     </div>
-                    <div className='controlPanelDoubleButton'>
+                    {/* <div className='controlPanelDoubleButton'>
                         <div className='controlPanelButtonContainerForm'>
                             <input type="number" name='amount' step="0.01"
                                 onChange={handleFormDataChange} placeholder='Enter the Amount in €' />
@@ -186,12 +196,23 @@ export default function CardMGT() {
                             <button onClick={handleMSGFile}>Create MSG File</button>
                             <p>{results[3] === null ? 'No Updates' : results[3] ? 'MSG File Created Successfully' : 'MSG File Creation Failed'}</p>
                         </div>
-                    </div>
-                    <div className='controlPanelButton'>
+                    </div> */}
+                    {/* <div className='controlPanelButton'>
                         <input type="text" name='testId' value={cardMGTDetails.testId}
                             placeholder='Enter the Test Number to Run' onChange={handleFormDataChange} />
                         <button onClick={handleRunSim}>Run Simulator</button>
                         <p>{results[4] === null ? 'No Updates' : 'Simulation Done'}</p>
+                    </div> */}
+                    <div className='XMLEditor'>
+                        <div className='XMLSelector'>
+                            <select>
+                                {laoderData.map(baseMessage =>
+                                    <option value={baseMessage} key={crypto.randomUUID}>{baseMessage}</option>)}
+                            </select>
+                            <button>Use Selected</button>
+                            <button>Create New</button>
+                        </div>
+                        <div></div>
                     </div>
                 </div>
             </div>
